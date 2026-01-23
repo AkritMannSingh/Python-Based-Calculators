02f935a182645ed0e0cd9ef0ece0dd673bd4f414
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-23 14:16:41 +0530

    Update basic_calc.py

--- Arithmetic-Calculator/basic_calc.py
+++ Arithmetic-Calculator/basic_calc.py
@@ -1,10 +1,12 @@
+print("Arithmetic Calculator🧮\n\n")
+
 print("Enter two numbers, below:-")
 num1 = int(input("Enter first number:"))
 num2 = int(input("Enter second number:"))
 
-print("You have selected operations such as - Addition, Subtraction, Multiplication, and Dividion only.")
+print("\nYou have basic Arithmetic operations - Addition, Subtraction, Multiplication, and Dividion only.\n")
 
-oper = str(input("Enter which operation you want to perform\n"))
+oper = str(input("Enter which operation you want to perform:"))
 
 if (oper == "Addition"):
   print("Addition of two numbers is:", num1 + num2)
