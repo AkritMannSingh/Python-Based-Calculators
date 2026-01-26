8d666d2d7bf7ae7bc1168c623e0308d532f70519
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-26 15:50:12 +0530

    Update enegy_finder.py

--- Other Calculators/enegy_finder.py
+++ Other Calculators/enegy_finder.py
@@ -1,1 +1,18 @@
+import math
+print("Hey! Welcome to a world of Physics...\n")
 
+print("This program calculates the Equivalent Energy of a given Mass using the famous Einstein's Equation E = mc^2\n")
+
+print("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n")
+
+mass = float(input("Enter the mass of the object: "))
+
+c = 3 * pow(10, 8)
+
+E = mass * c * c
+
+print("Equivalent Energy =", E, "Joules\n")
+
+print("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\n")
+
+print("Thank You for using this program...")
